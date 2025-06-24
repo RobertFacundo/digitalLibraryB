cbbb70cab6d55109483fe78a6d1462bc12e85246
Facundo Robert <robert.facundo@hotmail.com>
2025-06-24 17:05:20 -0300

    database connection

--- database.py
+++ database.py
@@ -3,12 +3,28 @@ from sqlalchemy.orm import declarative_base
 from sqlalchemy import MetaData
 import os
 from dotenv import load_dotenv
+import ssl
 
 load_dotenv()
 
-DATABASE_URL = os.getenv('DATABASE_URL').replace("postgresql://", "postgresql+asyncpg://")
+DATABASE_URL = os.getenv('DATABASE_URL')
 
-engine = create_async_engine(DATABASE_URL, echo=True)
+if 'localhost' in DATABASE_URL or "127.0.0.1" in DATABASE_URL:
+
+    async_database_url = DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://")
+    engine = create_async_engine(async_database_url, echo=True)
+else:
+    if DATABASE_URL.endswith("?sslmode=require"):
+        DATABASE_URL = DATABASE_URL.replace("?sslmode=require", "")
+    async_database_url = DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://")
+
+    ssl_context = ssl.create_default_context()
+
+    engine = create_async_engine(
+        async_database_url,
+        echo=True,
+        connect_args={'ssl': ssl_context}
+    )
 
 AsyncSessionLocal = async_sessionmaker(
     autocommit=False,
